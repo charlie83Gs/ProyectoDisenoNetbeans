fe8abae92298adab5de12efbea0e400c77798aac
Carlos <carlosgomezsoza@gmail.com>
2019-11-02 16:28:26 -0600

    request

---
+++ src/main/java/com/diseno/proyecto1diseno/model/Request.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.diseno.proyecto1diseno.model;
+
+/**
+ *
+ * @author Charlie
+ */
+public class Request {
+    
+}
