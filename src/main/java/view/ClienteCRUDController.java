cccdcdd895afbf580d465c01c44f1dbaec694e28
Bryan Hernandez Ortiz <malabaryan@hotmail.com>
2019-11-10 20:34:05 -0600

    cambios a cliente

--- src/main/java/view/ClienteCRUDController.java
+++ src/main/java/view/ClienteCRUDController.java
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
@@ -41,29 +42,30 @@ public class ClienteCRUDController implements Initializable {
     private TextField text_telefonoClienteCRUD;
     @FXML
     private TextField text_edadClienteCRUD;
+    @FXML
+    private TableColumn<?, ?> column_nombre;
+    @FXML
+    private TableColumn<?, ?> column_email;
+    @FXML
+    private TableColumn<?, ?> column_telefono;
 
     
-    @FXML
     private void handler_btn_CrearActualizarClienteCRUD(ActionEvent event) {
         System.out.println("You clicked me!");
     }
     
-    @FXML
     private void handler_btn_EliminarCliente(ActionEvent event) {
         System.out.println("You clicked me!");
     }
     
-    @FXML
     private void handler_btn_AtrasClienteCRUD(ActionEvent event) {
         System.out.println("You clicked me!");
     }
     
-    @FXML
     private void handler_btn_VerCliente(ActionEvent event) {
         System.out.println("You clicked me!");
     }
     
-    @FXML
     private void handler_btn_ModificarCliente(ActionEvent event) {
         System.out.println("You clicked me!");
     }
