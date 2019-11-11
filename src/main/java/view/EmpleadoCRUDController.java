6afc5625ea42e84ed98616d5a363a46adc1341a2
Kenneth Herrera <kfhv.24@gmail.com>
2019-11-11 00:52:10 -0600

    Finished EmpleadoCRUDController.

--- src/main/java/view/EmpleadoCRUDController.java
+++ src/main/java/view/EmpleadoCRUDController.java
@@ -5,14 +5,32 @@
  */
 package view;
 
+import com.diseno.proyecto1diseno.model.Employee;
+import controller.Payload;
+import controller.command.DeleteCommand;
+import controller.command.GetAllCommand;
+import controller.command.UpdateCommand;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -22,9 +40,9 @@ import javafx.scene.control.TextField;
 public class EmpleadoCRUDController implements Initializable {
 
     @FXML
-    private TableView<?> table_EmpleadoCRUD;
+    private TableView<Employee> table_EmpleadoCRUD;
     @FXML
-    private Button btn_CrearActualizarEmpleadoCRUD;
+    private Button btn_ActualizarEmpleadoCRUD;
     @FXML
     private Button btn_AtrasEmpleadoCRUD;
     @FXML
@@ -40,21 +58,75 @@ public class EmpleadoCRUDController implements Initializable {
     @FXML
     private TextField text_telefonoEmpleadoCRUD;
     @FXML
-    private TextField text_edadEmpleadoCRUD;
-
+    private TableColumn<Employee, String> column_nombre;
     @FXML
-    private void handler_btn_CrearActualizarEmpleadoCRUD(ActionEvent event) {
-        System.out.println("You clicked me!");
+    private TableColumn<Employee, String> column_email;
+    @FXML
+    private TableColumn<Employee, String> column_telefono;
+    @FXML
+    private TableColumn<Employee, String> column_id;
+    
+    private Employee tempEmployee;
+    
+    @FXML
+    private void handler_btn_ActualizarEmpleadoCRUD(ActionEvent event) {
+        if(tempEmployee != null){
+            Payload payload = new Payload();
+            
+            tempEmployee.setName(text_nombreEmpleadoCRUD.getText());
+            tempEmployee.setEmail(text_emailEmpleadoCRUD.getText());
+            tempEmployee.setPhone(text_telefonoEmpleadoCRUD.getText());
+            
+            payload.addContent("object", tempEmployee);
+            try {
+                UpdateCommand command = new UpdateCommand(payload);
+                command.execute();
+            } catch (Exception ex) {
+                Logger.getLogger(EmpleadoCRUDController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            text_nombreEmpleadoCRUD.clear();
+            text_emailEmpleadoCRUD.clear();
+            text_telefonoEmpleadoCRUD.clear();
+        }
+        
+        tempEmployee = null;
+        table_EmpleadoCRUD.setItems(employees());
     }
     
     @FXML
     private void handler_btn_AtrasEmpleadoCRUD(ActionEvent event) {
-        System.out.println("You clicked me!");
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/GUI/AdminMain.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Administración");
+            stage.setScene(new Scene(root, 450, 450));
+            stage.show();
+            // Hide this current window (if this is what you want)
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     @FXML
     private void handler_btn_EliminarEmpleadoCRUD(ActionEvent event) {
-        System.out.println("You clicked me!");
+        tempEmployee = table_EmpleadoCRUD.getSelectionModel().getSelectedItem();
+        
+        Payload payload = new Payload();
+        payload.addContent("id", tempEmployee.getId());
+        payload.addContent("class", Employee.class);
+        
+        try {
+            DeleteCommand<Employee> delete = new DeleteCommand(payload);
+            delete.execute();
+        } catch (Exception ex) {
+            Logger.getLogger(EmpleadoCRUDController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        tempEmployee = null;
+        table_EmpleadoCRUD.setItems(employees());
     }
     
     @FXML
@@ -64,7 +136,11 @@ public class EmpleadoCRUDController implements Initializable {
     
     @FXML
     private void handler_btn_ModificarEmpleadoCRUD(ActionEvent event) {
-        System.out.println("You clicked me!");
+        tempEmployee = table_EmpleadoCRUD.getSelectionModel().getSelectedItem();
+        
+        text_nombreEmpleadoCRUD.setText(tempEmployee.getName());
+        text_emailEmpleadoCRUD.setText(tempEmployee.getEmail());
+        text_telefonoEmpleadoCRUD.setText(tempEmployee.getPhone());
     }
     
     /**
@@ -72,7 +148,26 @@ public class EmpleadoCRUDController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        column_nombre.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        column_email.setCellValueFactory(new PropertyValueFactory<>("Email"));
+        column_telefono.setCellValueFactory(new PropertyValueFactory<>("Phone"));
+        column_id.setCellValueFactory(new PropertyValueFactory<>("Id"));
+        
+        table_EmpleadoCRUD.setItems(employees());
     }    
     
+    private ObservableList<Employee> employees() {
+        Payload payload = new Payload();
+        payload.addContent("class", Employee.class);
+        ArrayList<Employee> employees;
+        
+        try {
+            GetAllCommand<Employee> command = new GetAllCommand(payload);
+            employees = command.execute();
+        } catch (Exception ex) {
+            Logger.getLogger(EmpleadoCRUDController.class.getName()).log(Level.SEVERE, null, ex);
+            return FXCollections.observableArrayList();
+        }
+        return FXCollections.observableArrayList(employees);
+    }
 }
